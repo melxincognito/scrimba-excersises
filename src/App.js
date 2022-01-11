35cc260fe88e25ca564b32b8d928b7b4b29ed4bf
Mel Incognito <melxincognito@gmail.com>
2022-01-11 14:56:35 -0700

    finished HOC challenge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 
-import { withExtraPropAdded } from "./components/withExtraPropAdded";
+import { withFavoriteNumber } from "./components/withFavoriteNumber";
+
 function App(props) {
   console.log(props);
+
   return (
     <div>
       <h1>Hello world! </h1>
+      <h1> {props.favoriteNumber} </h1>
     </div>
   );
 }
 
-const PointlessHOC = withExtraPropAdded(App);
-export default PointlessHOC;
+const FavoriteNumberHOC = withFavoriteNumber(App);
+export default FavoriteNumberHOC;
+
+// Reference favoriteNumber as the props in order to have it display on the screen.
+// Not { withFavoriteNumber.props } or { props.withFavoriteNumber }
+// Reference the actual prop you NAMED inside of the HOC
---
+++ src/components/withFavoriteNumber.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/** A function that takes a component as its first argument and returns a
+ * new component that wraps the given component, providing extra capabilities
+ * to it.
+ * Challenge: Write a higher order component that passes a new prop to the given
+ * component called "favoriteNumber" and invludes your personal favorite number
+ * Then, in App.js, render that favorite number onto the screen
+ **/
+
+export function withFavoriteNumber(component) {
+  const Component = component;
+  return function (props) {
+    return <Component favoriteNumber={7} {...props} />;
+  };
+}
