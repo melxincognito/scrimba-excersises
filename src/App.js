17f4537efd628756578cbcc753b406189e82d732
Mel Incognito <melxincognito@gmail.com>
2022-01-12 17:39:03 -0700

    organized stuff. finished HOCs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import "./App.css";
+import React from "react";
+import Menu from "./components/hoc/Menu";
+import Favorite from "./components/hoc/Favorite";
 
-import { withFavoriteNumber } from "./components/withFavoriteNumber";
-
-function App(props) {
-  console.log(props);
-
+function App() {
   return (
     <div>
-      <h1>Hello world! </h1>
-      <h1> {props.favoriteNumber} </h1>
+      <Menu />
+      <hr />
+      <Favorite />
     </div>
   );
 }
 
-const FavoriteNumberHOC = withFavoriteNumber(App);
-export default FavoriteNumberHOC;
-
-// Reference favoriteNumber as the props in order to have it display on the screen.
-// Not { withFavoriteNumber.props } or { props.withFavoriteNumber }
-// Reference the actual prop you NAMED inside of the HOC
+export default App;
---
+++ src/HOCS/withToggler.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+// HOC is a function that takes a component as a parameter
+// and returns a new component wrapping the goven component
+// and supercharges it by giving it extra capabilities
+
+class Toggler extends Component {
+  state = {
+    on: false,
+  };
+
+  toggle = () => {
+    this.setState((prevState) => {
+      return {
+        on: !prevState.on,
+      };
+    });
+  };
+
+  render() {
+    const Component = this.props.component;
+    return (
+      <Component on={this.state.on} toggle={this.toggle} {...this.props} />
+    );
+  }
+}
+export function withToggler(component) {
+  return function (props) {
+    return <Toggler component={component} {...props} />;
+  };
+}
